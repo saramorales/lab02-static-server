e9362abcd81e4b7a0bffd94fb9152143d3eafff3
Sarai Morales <stephany.martell@gmail.com>
2016-07-07 10:18:15 -0500

    lab05 "Server con html fijo"

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // http
 var http= require("http");
+var fs= require('fs');
 //obteniendo informacion del entorno
 // De ejecucion con respecto al IP
 // y al puertoque debemos usar en 
 // nuestro server.
 var PORT= process.env.PORT || 3000;
-var IP= process.env.IP || '127.0.0.1';
-if (IP=='127.0.0.1'){
+var IP= process.env.IP || '192.168.1.41';
+if (IP=='192.168.1.41'){
     console.log(">------EJECUTANDO EN MODO LOCAL------");
 }
 // Crear un servidor basico
@@ -14,15 +15,20 @@ var server= http.createServer(function(req, res){
     // Armar la respuesta en http
     // Armar un encabezado http
     res.writeHead(200, {
-        "Content-Type": "text/plain", 
+        "Content-Type": "text/html", 
         "Server": "ITGAM@4.2.4"
     });
-    // Enviamos la respuesta 
-    res.write("Hola Sara");
-    // Cerrar la conexion 
-    res.end();
+    // Lectura del archivo a servir
+    fs.readFile('./static./index.html',
+    'utf8',function(err, content){
+        if(err){
+            res.write("<h1>ERROR DE LECTURA</H1>");
+        }else{
+            res.end(content);
+        }
+    });
 });
 // Poner a trabajar al server 
 server.listen(PORT,IP,function (){
     console.log(`> Server listening @http://${IP}:${PORT} ...`);
-} );
+});
